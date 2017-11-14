13b4d0ab702f7836d8249ca688b3dfdec3835a99
Tai-Iro <daniel.e.schrader.jr@gmail.com>
2017-11-13 22:49:22 -0500

    changed a bit to make it work
    Changes are in comments. Line 69 of ReportDetailController, line 56 of LoginController, and the view is a duplicate of the normal one with the button made invisible (line 37).

---
+++ WilderTest Versions/LoginController.java
@@ -0,0 +1,171 @@
+package com.wildertest.controller;
+
+import application.WilderTestDBConfig;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class LoginController{
+    @FXML private Label loginTitleLabel;
+    @FXML public TextField usernameTextField, passwordTextField;
+    @FXML private Button loginButton;
+    
+    private Stage stage;
+    private AnchorPane root;
+    private Scene scene;
+    //private Connection conn; //never used
+    private static String permission = "";
+    private static String userID = "";
+
+    public void login(ActionEvent event) throws Exception {
+    	/*String*/ permission ="";
+
+    	checkUsersTable(); //for testing
+    	
+        try {
+            permission = getPermission(usernameTextField.getText(), String.valueOf(hashPass(passwordTextField.getText())));
+
+            //conn = WilderTestDBConfig.getConnection();//shouldn't need to connect to the database here
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+    	
+    	if(checkHashPass() == true) {
+    		stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+    		if(permission.equals("1"))	
+            {
+            	root = FXMLLoader.load(getClass().getResource("../view/AdminGUI.fxml"));
+            	scene = new Scene(root);
+            	stage.setScene(scene);
+            }
+    		else if(permission.equals("0")){
+    			root = FXMLLoader.load(getClass().getResource("../view/ReportDetailView3.fxml"));//now goes to new one
+                scene = new Scene(root);
+                stage.setScene(scene);
+    		}
+    		else System.out.println("login failed");
+
+        } else System.out.println("login failed");
+        
+        
+
+    }
+    
+    public static int hashPass(String password) {
+        int hash = 7;
+        for (int i = 0; i < password.length(); i++) {
+            hash = hash*31 + password.charAt(i);
+        }
+        System.out.println("Password: " + password + ", hash: " + hash);
+        return hash;
+    }
+
+    public boolean checkHashPass(){
+    	//all of this is already taken care of
+        //String permission ="";
+//        try {
+//            permission = getPermission(usernameTextField.getText(), String.valueOf(hashPass(passwordTextField.getText())));
+//            System.out.println("The permission level is "+ permission);
+//
+//            //conn = WilderTestDBConfig.getConnection(); //shouldn't need to connect to the database here
+//
+//        } catch (Exception e) {
+//        	e.printStackTrace();
+//        }
+        if (permission.equals("1") || permission.equals("0")) {
+            return true;
+        }
+            else return false;
+    }
+    /*
+SELECT users.privilege
+FROM users
+WHERE users.password = 1449706402
+AND
+users.username = 'Admin'
+GROUP BY users.userID;
+
+ */
+    public static String getPermission(String username, String password) {
+//    	Statement statement = null;
+//    	ResultSet resultSet = null;
+//    	Connection connection = null;
+        try( //Need to either put these here, or close them all in a finally statement
+    		Connection connection = WilderTestDBConfig.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT users.privilege, users.userID FROM users Where users.password ="+password+
+            " AND users.username ="+"'"+username+"'"+" GROUP BY users.userID;");
+            ResultSet resultSet = statement.executeQuery();
+        ){
+            while (resultSet.next()) {
+                permission = String.valueOf(resultSet.getInt("privilege"));
+                userID = String.valueOf(resultSet.getInt("userID"));
+            }
+            //connection.close();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+//        finally { //example of closing them in a finally statement
+//        	try { resultSet.close(); } catch (Exception e) { /* ignored */ }
+//            try { statement.close(); } catch (Exception e) { /* ignored */ }
+//            try { connection.close(); } catch (Exception e) { /* ignored */ }
+//        }
+        System.out.println("the userID is: "+userID);
+        System.out.println("privilege @getPermission is "+ permission);
+        return permission;
+    }
+
+    public void checkUsersTable()
+    {
+    	//testing\/
+    	//below is for testing to see what is in the database
+    	Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn2 = null;
+        ResultSetMetaData rsmd = null;
+        try{
+        	conn2 = WilderTestDBConfig.getConnection();
+        	stmt = conn2.createStatement();
+            
+	    	rs = stmt.executeQuery("SELECT * FROM users");// "SELECT * FROM reports"
+	    	rsmd = rs.getMetaData();
+	    	int columnCount = rsmd.getColumnCount();
+	
+	    	System.out.println(columnCount);
+	    	// The column count starts from 1
+	    	for (int i = 1; i <= columnCount; i++ ) {
+	    	  String name = rsmd.getColumnName(i);
+	    	  // Do stuff with name
+	    	  System.out.println(name + " | " + rsmd.getColumnLabel(i) + " | " + rsmd.getColumnType(i) + " | " + rsmd.getColumnTypeName(i));
+	    	}
+	    	while(rs.next())
+	    	{
+	    		System.out.println(rs.getInt("userID") + " | " + rs.getString("privilege") + " | " + rs.getString("username")+ " | " + rs.getString("password"));
+	    	}
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString());
+        }
+        finally {
+        	try { rs.close(); } catch (Exception e) { /* ignored */ }
+            try { stmt.close(); } catch (Exception e) { /* ignored */ }
+            try { conn2.close(); } catch (Exception e) { /* ignored */ }
+       }
+        //^testing^
+    }
+    
+}
+
---
+++ WilderTest Versions/ReportDetailController.java
@@ -0,0 +1,237 @@
+package com.wildertest.controller;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.*;
+
+import com.wildertest.model.Report;
+import com.wildertest.model.Station;
+
+import application.WilderTestDBConfig;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ReportDetailController extends LoginController {
+    @FXML private Label titleLabel;
+    @FXML private Label reportsLabel;
+    @FXML private Label stationLabel;
+
+    @FXML private Pane backgroundPane;
+
+    @FXML private ListView<Station> stationListView;
+
+    @FXML private TableView<Report> reportsTable;
+    @FXML private TableColumn<Report, String> dateColumn;
+    @FXML private TableColumn<Report, Double> tempColumn;
+    @FXML private TableColumn<Report, Double> pressureColumn;
+    @FXML private TableColumn<Report, Double> rainColumn;
+    @FXML private TableColumn<Report, Double> windSpeedColumn;
+    @FXML private TableColumn<Report, String> windDirectionColumn;
+    @FXML private TableColumn<Report, String> conditionsColumn;
+    @FXML private Button backButton;
+
+    private Stage stage;
+    private AnchorPane root;
+    private Scene scene;
+    
+    private static String permission = "";
+
+
+    @FXML
+    public void initialize() {
+    	stationListView.setItems(getStationList());
+    	
+    	
+    	dateColumn.setCellValueFactory(new PropertyValueFactory<Report, String>("timeCreated"));
+    	tempColumn.setCellValueFactory(new PropertyValueFactory<Report, Double>("temperature"));
+    	pressureColumn.setCellValueFactory(new PropertyValueFactory<Report, Double>("pressure"));
+        rainColumn.setCellValueFactory(new PropertyValueFactory<Report, Double>("rain"));
+        windSpeedColumn.setCellValueFactory(new PropertyValueFactory<Report, Double>("windSpeed"));
+        windDirectionColumn.setCellValueFactory(new PropertyValueFactory<Report, String>("windDirection"));
+        conditionsColumn.setCellValueFactory(new PropertyValueFactory<Report, String>("conditions"));
+        //backButton.setVisible(false); //commented out
+        
+    	reportsTable.setItems(getReportsList());
+    	
+    	System.out.println("\n------------------------------------------------------------------\n");
+        
+    	testDB();
+    }
+    public void backToAdmin(ActionEvent event) throws Exception {
+    			stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            	root = FXMLLoader.load(getClass().getResource("../view/AdminGUI.fxml"));
+            	scene = new Scene(root);
+            	stage.setScene(scene);
+           }
+        /*if(permission.equals("1"))
+        {
+        	backButton.setVisible(true);
+        	stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        	root = FXMLLoader.load(getClass().getResource("../view/AdminGUI.fxml"));
+        	scene = new Scene(root);
+        	stage.setScene(scene);
+        }*/
+
+    public boolean visibleButton()
+    {
+    	permission ="";
+    	boolean visible = false;
+    	
+        try {
+             permission = getPermission(usernameTextField.getText(), String.valueOf(hashPass(passwordTextField.getText())));
+
+        } catch (Exception e) {
+         	System.out.println("back button not working");
+         	e.printStackTrace();
+         }
+        if(permission.equals("1"))
+        {
+        	visible = true;
+        	backButton.setVisible(visible);
+        }
+        else
+        {
+        	backButton.setVisible(visible);
+        	backButton.setDisable(visible);
+        }
+		return visible;
+    	
+    
+    }
+
+    public ObservableList<Station>/*<String>*/  getStationList()
+    {
+    	ObservableList<Station>/*<String>*/ stations = FXCollections.observableArrayList();
+
+        String SQLQuery = "select stationID, stationName, latitude, longitude from stations";
+        
+//		ResultSet rs = null;
+
+		try(
+				Connection conn = WilderTestDBConfig.getConnection();
+				PreparedStatement displayprofile = conn.prepareStatement(SQLQuery);
+				ResultSet rs = displayprofile.executeQuery();
+		){
+			//displayprofile.setInt(1, cutomerId);
+//			rs = displayprofile.executeQuery();
+
+			// check to see if receiving any data
+			while (rs.next()){
+				stations.add(new /*String*/Station(rs.getInt("stationID"), rs.getString("stationName")/*.toString()*/, 
+						rs.getDouble("latitude"), rs.getDouble("longitude")));
+
+			}
+		}catch(SQLException ex){
+			WilderTestDBConfig.displayException(ex);
+			return null;
+		}/*finally{
+			if(rs != null){
+				//rs.close();
+			}
+		}*/
+        return stations;
+    }
+    
+    public void mouseClickedOnListView()
+    {
+        //
+    	if(stationListView.getSelectionModel().getSelectedItem() != null)
+        {
+    		reportsTable.getItems().clear();
+    		reportsTable.setItems(getReportsList());
+    		//reportsTable.refresh();
+        }
+    }
+    
+    public ObservableList<Report>/*<String>*/  getReportsList()
+    {
+    	ObservableList<Report>/*<String>*/ reports = FXCollections.observableArrayList();
+
+        String SQLQuery = "select reportID, stationID, timestamp, temp, pressure, rain, windSpeed, windDirection, conditions from reports";
+
+        //If something is selected
+        if(stationListView.getSelectionModel().getSelectedItem() != null)
+        {
+        	SQLQuery += " where stationID = " + stationListView.getSelectionModel().getSelectedItem().getStationID();
+        	System.out.println(SQLQuery);
+        }
+        
+		//ResultSet rs = null;
+
+		try(
+				Connection conn = WilderTestDBConfig.getConnection();
+				PreparedStatement displayprofile = conn.prepareStatement(SQLQuery);
+				ResultSet rs = displayprofile.executeQuery();
+		){
+			//displayprofile.setInt(1, cutomerId);
+			//rs = displayprofile.executeQuery();
+
+			// check to see if receiving any data
+			while (rs.next()){
+				reports.add(new Report(rs.getInt("reportID")/*.toString()*/, rs.getInt("stationID")/*.toString()*/, rs.getString("timestamp"),
+						rs.getDouble("temp"), rs.getDouble("pressure"), rs.getDouble("rain"),
+						rs.getDouble("windSpeed"), rs.getString/*Double*/("windDirection"), rs.getString("conditions")));
+
+			}
+		}catch(SQLException ex){
+			WilderTestDBConfig.displayException(ex);
+			return null;
+		}/*finally{
+			if(rs != null){
+				//rs.close();
+			}
+		}*/
+        return reports;
+    }
+    
+    private void testDB()
+    {
+    	//below is for testing to see what is in the database
+    	Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        ResultSetMetaData rsmd = null;
+        try{
+        	conn = WilderTestDBConfig.getConnection();
+        	stmt = conn.createStatement();
+	    	rs = stmt.executeQuery("SELECT * FROM stations");// "SELECT * FROM reports"
+	    	rsmd = rs.getMetaData();
+	    	int columnCount = rsmd.getColumnCount();
+	
+	    	System.out.println(columnCount);
+	    	
+	    	for (int i = 1; i <= columnCount; i++ ) { // The column count starts from 1
+	    	  String name = rsmd.getColumnName(i);// Do stuff with name
+	    	  System.out.println(name + " | " + rsmd.getColumnLabel(i) + " | " + rsmd.getColumnType(i) + " | " + rsmd.getColumnTypeName(i));
+	    	}
+//	    	rs.first();
+//	    	System.out.println(rs.getInt("reportID") + "\t" + rs.getString("timestamp") + "\t" + rs.getDouble("temp"));
+//	    	rs.last();
+//	    	System.out.println(rs.getInt("reportID") + "\t");
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString());
+        }
+        finally {
+        	try { rs.close(); } catch (Exception e) { /* ignored */ }
+            try { stmt.close(); } catch (Exception e) { /* ignored */ }
+            try { conn.close(); } catch (Exception e) { /* ignored */ }
+       }
+    }
+}
